fb9df5c7bec129a1c72cacfde5d6e9a8c12fe1e1
Robertpham912 <robertpham8822@gmail.com>
2026-01-22 22:39:23 +0700

    Enhance main.js with IPC and window settings
    Added IPC functionality to run system commands and updated window dimensions.
    
    Signed-off-by: Robertpham912 <robertpham8822@gmail.com>

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { exec } = require('child_process'); // Thư viện để chạy lệnh hệ thống
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: "Web của Phúc - Admin Gia Phúc"
-  })
-  win.loadFile('index.html') // Nó sẽ mở file web của cậu lên
+    width: 900,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow)
+// Ví dụ một lệnh "tweak" - Dọn dẹp bộ nhớ đệm hệ thống
+ipcMain.on('run-tweak', (event, arg) => {
+  exec('del /q/f/s %TEMP%\\*', (err, stdout, stderr) => {
+    if (err) {
+      event.reply('tweak-reply', 'Lỗi rồi đại ca ơi!');
+      return;
+    }
+    event.reply('tweak-reply', 'Đã dọn dẹp xong! Máy mượt như lụa.');
+  });
+});
+
+app.whenReady().then(createWindow);
